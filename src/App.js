cf02d51dca85fe30f3bbc7d949634f6d049f3ca4
Jayne Valverde <jaynevalverde@gmail.com>
2023-09-09 20:01:15 -0500

    successfull deployment of homepage

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import './global-styles/global.css';
-import Header from './components/Header/Header';
-import Homepage from './pages/Homepage/Homepage';
-import Contact from './pages/Contact/Contact';
-import Projects from './pages/Projects/Proejcts';
-import Footer from './components/Footer/Footer';
+import Homepage from './components/Homepage/Homepage';
 
-// Commented out (Default Function App)
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 function App() {
   return (
-
-    <div className="App">
-      <Router>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Homepage />} />
-          <Route path='/projects' element={<Projects />} />
-          <Route path='/contact' element={<Contact />} />
-        </Routes>
-        <Footer />
-      </Router>
-    </div>
-
-
-  );
+  <div className='App'>
+    <Homepage />
+  </div>
+  )
 }
+
 
 export default App;
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Homepage.css";
+
+function Homepage() {
+    return (
+        <div className="homepage-containter">
+            <div className="hero-section">
+                <h1>JAYNÉ M. VALVERDE</h1>
+            </div>
+            <div className="port-statment">
+                <h2>Welcome Welcome Welcome Welcome</h2>
+                <p>I am a Junior Web Developer with a Bachelor's degree in Fine Arts.</p>
+                <p>Welcome Welcome Welcome Welcome</p>
+            </div>
+        </div>
+    )
+}
+
+
+export default Homepage;
